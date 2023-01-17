01f3cfd521c6bd9d79f729a305bf94d4aaf7585a
mcmpdemo <mariobv@kyndryl.com>
2023-01-17 09:05:53 -0600

    change the dora metrics automation branch name

--- github_data_load/dora_metrics_data_mondays.py
+++ github_data_load/dora_metrics_data_mondays.py
@@ -36,14 +36,14 @@ def main():
         raise Exception("Unable to create issue")
 
     #Commit something to 'master'
-    _, errorMessage = create_commit( repoConnection, "Autmatic commit from jenkins dora metrics", "release-2023", fileToLog="dora_metrics_log.log", logErrors=True  )
+    _, errorMessage = create_commit( repoConnection, "Autmatic commit from jenkins dora metrics", "automation-branch", fileToLog="dora_metrics_log.log", logErrors=True  )
     if errorMessage:
         
         exit(1)
 
     #create PR from 'release-2023' to 'master'
     bodyDescription = f"https://github.com/{setup['repo']}/issues/{storyIssueNumber}"
-    create_pull_request( repoConnection, headBranch="release-2023", baseBranch="master", body=bodyDescription, logErrors=True )
+    create_pull_request( repoConnection, headBranch="automation-branch", baseBranch="master", body=bodyDescription, logErrors=True )
 
 
 
