1702a1878a603f2ead16b6698450d2f3799993f0
“bridge-demo” <mariobv@kyndryl.com>
2023-11-09 14:59:38 -0600

    DI Release management/ adding sev1, sev2 and user story issues

--- github_data_load/dora_metrics_data_fridays.py
+++ github_data_load/dora_metrics_data_fridays.py
@@ -16,11 +16,22 @@ def parser( ) -> dict:
 def main():
     setup =  parser()
     repoConnection = Github( setup["gitHubToken"] ).get_repo( setup["repo"] )
-    releaseNumber = time.strftime("%Y.%m.%d") #Using number of the week as release counter
+    releaseNumber = time.strftime("%Y.%m.%d") 
     releaseBranchName = f"release-{releaseNumber}"
     create_branch( repoConnection=repoConnection, newBranchName=releaseBranchName )
+    
+    ## Release management data
+    labelsForSev1 = [f"found_{releaseBranchName}", "sev1", "bug", ]
+    create_issue(repoConnection=repoConnection, labels=labelsForSev1, title=f"Severity 1 issue - release {releaseBranchName}" )
+
+    labelsForSev2 = [f"found_{releaseBranchName}", "sev2", "bug", "User Story" ]
+    create_issue( repoConnection=repoConnection, labels=labelsForSev2, title=f"Severity 2 issue - release {releaseBranchName}" )
+    ## /Release management data
+
     close_issues(repoConnection=repoConnection, Labelfilters=["User Story", "Epic"])
+    close_issues(repoConnection=repoConnection, Labelfilters=labelsForSev1)
+    close_issues(repoConnection=repoConnection, Labelfilters=labelsForSev2)
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     main()
