cb14d3d2aa12d77ccc6b9c04d7ea5eb45bf3b9b5
“bridge-demo” <mariobv@kyndryl.com>
2023-11-15 09:57:03 -0600

    release management / printing logs and leaving issues open

--- github_data_load/dora_metrics_data_fridays.py
+++ github_data_load/dora_metrics_data_fridays.py
@@ -21,16 +21,22 @@ def main():
     create_branch( repoConnection=repoConnection, newBranchName=releaseBranchName )
     
     ## Release management data
+    print(f"Creating sev1 for {releaseBranchName}")
     labelsForSev1 = [f"found_{releaseBranchName}", "sev1", "bug", ]
-    create_issue(repoConnection=repoConnection, labels=labelsForSev1, title=f"Severity 1 issue - release {releaseBranchName}" )
-
+    sev1_issueNumber = create_issue(repoConnection=repoConnection, labels=labelsForSev1, title=f"Severity 1 issue - release {releaseBranchName}" )
+    print(f"issue number -> {sev1_issueNumber}")
+    
+    print(f"Creating sev2 for {releaseBranchName}")
     labelsForSev2 = [f"found_{releaseBranchName}", "sev2", "bug", "User Story" ]
-    create_issue( repoConnection=repoConnection, labels=labelsForSev2, title=f"Severity 2 issue - release {releaseBranchName}" )
+    sev2_issueNumber = create_issue( repoConnection=repoConnection, labels=labelsForSev2, title=f"Severity 2 issue - release {releaseBranchName}" )
+    print(f"issue number -> {sev2_issueNumber}")
+
     ## /Release management data
 
     close_issues(repoConnection=repoConnection, Labelfilters=["User Story", "Epic"])
-    close_issues(repoConnection=repoConnection, Labelfilters=labelsForSev1)
-    close_issues(repoConnection=repoConnection, Labelfilters=labelsForSev2)
+    # Not closing the sev issues
+    # close_issues(repoConnection=repoConnection, Labelfilters=labelsForSev1)
+    # close_issues(repoConnection=repoConnection, Labelfilters=labelsForSev2)
 
 
 if __name__ == "__main__":
